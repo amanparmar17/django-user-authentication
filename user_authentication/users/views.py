18ce732be85a34a68ebc801300bc318dbe1f4b53
amanparmar17 <aman.parmar17@gmail.com>
2019-01-29 14:16:01 +0530

    adding new templates and login functionality

--- user_authentication/users/views.py
+++ user_authentication/users/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+
 
 def index(request):
 	return render(request,'users/index.html')
+
+def register(request):
+	form=UserCreationForm()
+	return render(request,'registration/register.html',{'form':form})
